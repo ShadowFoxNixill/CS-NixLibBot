2ea11e4b04df43de7d224193b2ecb6b0aa76c495
ShadowFoxNixill <github@nix.nixill.net>
2020-02-27 10:44:33 -0500

    git init commit push

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NixLibBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
